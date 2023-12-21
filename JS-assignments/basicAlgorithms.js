c38f1c92cf76a0c74b4112798deb005b1855e319
gcarlin01 <giancarlo.carlin01@gmail.com>
2023-12-21 17:13:38 -0500

    where do I belong - sorting algorithm JS

--- JS-assignments/basicAlgorithms.js
+++ JS-assignments/basicAlgorithms.js
@@ -206,3 +206,16 @@ function bouncer(arr) {
   return cleanArray;
 }
 console.log(bouncer([7, "ate", "", false, 9]));
+
+// Where do I belong - sorting method
+function getIndexToIns(arr, num) {
+  //Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+  //For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+  //Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+  // First I push num into the array
+  arr.push(num);
+  // Second I sort the numbers from lowest to highest using a comparisson arrow function for ascending order
+  arr.sort((a, b) => a - b);
+  return arr.indexOf(num);
+}
+console.log(getIndexToIns([2, 5, 10], 15));
