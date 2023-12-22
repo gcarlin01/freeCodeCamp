3da32c994f84771409d38688e50a90676075b7fc
gcarlin01 <giancarlo.carlin01@gmail.com>
2023-12-21 21:04:40 -0500

    mutations algorithm JS

--- JS-assignments/basicAlgorithms.js
+++ JS-assignments/basicAlgorithms.js
@@ -219,3 +219,20 @@ function getIndexToIns(arr, num) {
   return arr.indexOf(num);
 }
 console.log(getIndexToIns([2, 5, 10], 15));
+
+// Mutations
+function mutation(arr) {
+  // Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+  // For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+  // The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+  // Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+  let arr1 = arr[0].toLowerCase();
+  let arr2 = arr[1].toLowerCase();
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(mutation(["hello", "hey"]));
