3113c96f42d98f4b8446944821aa9d16066e8d31
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-03 21:24:08 -0500

    seek and destroy JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -30,3 +30,37 @@ console.log(
     ["diorite", "andesite", "grass", "dirt", "dead shrub"]
   )
 );
+
+// Seek and Destroy
+// You will be provided with an initial array as the first argument to the destroyer function, followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+// The function must accept an indeterminate number of arguments, also known as a variadic function. You can access the additional arguments by adding a rest parameter to the function definition or using the arguments object.
+//function destroyer(arg) {
+function destroyer(initialArray, ...args) {
+  let arraychecker = initialArray.filter((element) => !args.includes(element));
+  return arraychecker;
+}
+console.log(
+  destroyer(
+    [
+      "possum",
+      "trollo",
+      12,
+      "safari",
+      "hotdog",
+      92,
+      65,
+      "grandma",
+      "bugati",
+      "trojan",
+      "yacht",
+    ],
+    "yacht",
+    "possum",
+    "trollo",
+    "safari",
+    "hotdog",
+    "grandma",
+    "bugati",
+    "trojan"
+  )
+);
