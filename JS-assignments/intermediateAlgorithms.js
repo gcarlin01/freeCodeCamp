26ab64f39e16a8db50e9a459705713a2e036edf3
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-03 23:59:12 -0500

    pig latin JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -96,3 +96,26 @@ function spinalCase(str) {
 console.log(spinalCase("This Is Spinal Tap"));
 console.log(spinalCase("thisIsSpinalTap"));
 console.log(spinalCase("The_Andy_Griffith_Show"));
+
+// Pig Latin
+// Pig Latin is a way of altering English Words. The rules are as follows:
+// If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+// If a word begins with a vowel, just add way at the end.
+// Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+function translatePigLatin(str) {
+  let startsVowel = /^[aeiou]/;
+  let noVowel = /^[^aeiou]+$/;
+  if (str.match(startsVowel)) {
+    return str + "way";
+  } else {
+    if (str.match(noVowel)) {
+      return str + "ay";
+    } else {
+      return str.replace(/([^aeiou]+)([a-z]+)/g, "$2$1" + "ay");
+    }
+  }
+}
+console.log(translatePigLatin("rhythm"));
+console.log(translatePigLatin("paragraphs"));
+console.log(translatePigLatin("california"));
+console.log(translatePigLatin("algorithm"));
