4e0d3a6f62fda23c7488f22605ec5ac6ca7580de
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-05 20:17:32 -0500

    sorted union JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -182,3 +182,16 @@ function fearNotLetter(str) {
 }
 console.log(fearNotLetter("abce"));
 console.log(fearNotLetter("stvwx"));
+
+// Sorted Union
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+// Check the assertion tests for examples.
+function uniteUnique(first, ...arr) {
+  let joined = first.concat(...arr);
+  let filtered = joined.filter((elem, i, array) => i == array.indexOf(elem));
+  return filtered;
+}
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
