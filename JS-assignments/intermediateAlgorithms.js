518021c694465b8d3d7b59ea2fc246cdc2222550
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-06 19:51:40 -0500

    sum all primes JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -236,3 +236,30 @@ function sumFibs(num) {
   return sum;
 }
 console.log(sumFibs(4));
+
+// Sum all primes
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+// Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+function primeChecker(n) {
+  if (n < 2) {
+    return false;
+  } else {
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+function sumPrimes(num) {
+  let primeSum = 2;
+  for (let i = 3; i <= num; i++) {
+    if (primeChecker(i)) {
+      primeSum += i;
+    }
+  }
+  return primeSum;
+}
+console.log(sumPrimes(10)); // primes = 2, 3, 5, 7 sum = 17
+console.log(sumPrimes(977)); // sum = 73156
