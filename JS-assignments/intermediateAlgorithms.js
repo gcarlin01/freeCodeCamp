677b7ce99c5edbb323a440e7b54e11acb2b35c6d
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-10 21:01:54 -0500

    Binary Agents JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -345,3 +345,18 @@ console.log(steamrollArray([1, [2], [3, [[4]]]]));
 console.log(steamrollArray([[["a"]], [["b"]]]));
 
 // Binary Agents
+// Return an English translated sentence of the passed binary string.
+// The binary string will be space separated.
+function binaryAgent(str) {
+  let splitter = str.split(" ");
+  let convert = splitter.map((element) =>
+    String.fromCharCode(parseInt(element, 2))
+  );
+  let englishTranslated = convert.join("");
+  return englishTranslated;
+}
+console.log(
+  binaryAgent(
+    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+  )
+);
