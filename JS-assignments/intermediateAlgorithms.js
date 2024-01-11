f42c24cb3f2b2884ae3f345462b93f615fb0b5e0
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-10 20:59:00 -0500

    Drop it JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -292,3 +292,35 @@ function smallestCommons(arr) {
 }
 console.log(smallestCommons([5, 1]));
 console.log(smallestCommons([23, 18]));
+
+// Drop it
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr.slice(i);
+    }
+  }
+  return [];
+}
+console.log(
+  dropElements([1, 2, 3], function (n) {
+    return n < 3;
+  })
+);
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n > 5;
+  })
+);
+console.log(
+  dropElements([0, 1, 0, 1], function (n) {
+    return n === 1;
+  })
+);
+console.log(
+  dropElements([1, 2, 3, 9, 2], function (n) {
+    return n > 2;
+  })
+);
