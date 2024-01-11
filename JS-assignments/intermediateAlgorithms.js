905ea6176483101b9ea3530baf8fbd0cfd7afcfe
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-11 00:34:10 -0500

    Arguments optional JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -380,3 +380,26 @@ console.log(
     "isBot"
   )
 );
+
+// Arguments optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
+function addTogether() {
+  let [x, y] = arguments;
+  function oneArg(y) {
+    return addTogether(x, y);
+  }
+  if (typeof x == "number") {
+    if (typeof y == "number") {
+      return x + y;
+    }
+    if (arguments.length == 1) {
+      return oneArg;
+    }
+  }
+}
+console.log(addTogether(2, 3));
