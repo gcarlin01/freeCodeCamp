646de2dc079254ecaaa77e77996c7ca5762d4e76
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-11 22:47:42 -0500

    Make a person JS

--- JS-assignments/intermediateAlgorithms.js
+++ JS-assignments/intermediateAlgorithms.js
@@ -403,3 +403,37 @@ function addTogether() {
   }
 }
 console.log(addTogether(2, 3));
+
+// Make a person
+// Fill in the object constructor with the following methods below:
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// setFullName(first, last)
+// Run the tests to see the expected output for each method. These methods must be the only available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
+const Person = function (first, last) {
+  let firstName = first;
+  let lastName = last;
+  this.getFirstName = function () {
+    return firstName;
+  };
+  this.getLastName = function () {
+    return lastName;
+  };
+  this.getFullName = function () {
+    return this.getFirstName() + " " + this.getLastName();
+  };
+  this.setFirstName = function (first) {
+    return (firstName = first);
+  };
+  this.setLastName = function (last) {
+    return (lastName = last);
+  };
+  this.setFullName = function (first, last) {
+    this.setFirstName(first);
+    this.setLastName(last);
+    return this.getLastName();
+  };
+};
