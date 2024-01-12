f623181f418208402107af5d3bd138956db5f4e2
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-01-12 00:49:41 -0500

    Roman numeral converter JS

--- JS-assignments/finalProjects.js
+++ JS-assignments/finalProjects.js
@@ -10,3 +10,36 @@ function palindrome(str) {
   return alphanumericalOnly == reversed;
 }
 palindrome("A man, a plan, a canal. Panama");
+
+// Roman numeral converter
+// Convert the given number into a roman numeral.
+// All roman numerals answers should be provided in upper-case.
+function convertToRoman(num) {
+  let romanNumeral = "";
+  let romanValues = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" },
+  ];
+  for (let i = 0; i < romanValues.length; i++) {
+    while (num >= romanValues[i].value) {
+      romanNumeral += romanValues[i].numeral;
+      num -= romanValues[i].value;
+    }
+  }
+  return romanNumeral;
+}
+console.log(convertToRoman(36));
+console.log(convertToRoman(83));
+console.log(convertToRoman(99));
+console.log(convertToRoman(3999));
